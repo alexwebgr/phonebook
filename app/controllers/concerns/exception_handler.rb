c1e8253af07e4fae5301d3568bfffb8a5a6719e8
alexwebgr <websurfer992@gmail.com>
2019-07-21 09:31:11 +0100

    removed extra exception handlers

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -10,41 +10,27 @@ module ExceptionHandler
   
   included do
     # Define custom handlers
-    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
     rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
-    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
-    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
-    rescue_from ExceptionHandler::ExpiredSignature, with: :four_ninety_eight
-    rescue_from ExceptionHandler::DecodeError, with: :four_zero_one
+    rescue_from ExceptionHandler::MissingToken, with: :unprocessable_entity
+    rescue_from ExceptionHandler::InvalidToken, with: :unprocessable_entity
+    rescue_from ExceptionHandler::ExpiredSignature, with: :unauthorized_request
+    rescue_from ExceptionHandler::DecodeError, with: :unauthorized_request
 
-    rescue_from ActiveRecord::RecordNotFound do |e|
-     render json: { message: e.message }, status: :not_found
-    end
-
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      render json: { message: e.message }, status: :unprocessable_entity
-    end
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
   end
 
   private
 
-  # JSON response with message; Status code 422 - unprocessable entity
-  def four_twenty_two(e)
-   render json: { message: e.message }, status: :unprocessable_entity
-  end
- 
-# JSON response with message; Status code 401 - Unauthorized
-  def four_ninety_eight(e)
-    render json: { message: e.message }, status: :invalid_token
+  def unprocessable_entity(e)
+    render json: { message: e.message }, status: :unprocessable_entity
   end
 
-  # JSON response with message; Status code 401 - Unauthorized
-  def four_zero_one(e)
-    render json: { message: e.message }, status: :invalid_token
-  end
-
-   # JSON response with message; Status code 401 - Unauthorized
   def unauthorized_request(e)
     render json: { message: e.message }, status: :unauthorized
+  end
+
+  def not_found(e)
+    render json: { message: e.message }, status: :not_found
   end
 end
